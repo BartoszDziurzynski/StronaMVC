fb64fb8a7e8625d8c47079ed3ca6b556e9a6482e
TinkyWinkyPo <bartosz.dziurzynski@gmail.com>
2017-11-16 17:28:32 +0100

    no message

--- StronaMVC/StronaMVC/Controllers/HomeController.cs
+++ StronaMVC/StronaMVC/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace StronaMVC.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Brak APlikacji xD";
 
             return View();
         }
